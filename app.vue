8465771a58561b54323c20bbf317080e49463875
Paul CONVERT <paul.convert@epitech.eu>
2025-12-10 16:00:22 +0100

    Ajoute des images pour le partage sur les réseaux
    Ajoute les balises meta og:image et twitter:image
    pour améliorer le visuel lors du partage sur les réseaux
    sociaux.

--- app.vue
+++ app.vue
@@ -13,6 +13,16 @@
         name: 'viewport',
         content: 'width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes',
       },
+      {
+        property: 'og:image',
+        content:
+          'https://plaumondpicture.s3.eu-west-3.amazonaws.com/authors/Work+of+Identity+from+ID+Laumond.jpg',
+      },
+      {
+        name: 'twitter:image',
+        content:
+          'https://plaumondpicture.s3.eu-west-3.amazonaws.com/authors/Work+of+Identity+from+ID+Laumond.jpg',
+      },
     ],
     link: [
       {
